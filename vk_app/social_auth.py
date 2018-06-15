c2b1546569deadbd1c0b2ec6a15f007cc2952159
petr3460 <petr3460@gmail.com>
2018-06-15 17:38:30 +0300

    Update social_auth.py

--- vk_app/social_auth.py
+++ vk_app/social_auth.py
@@ -1,9 +1,7 @@
-import pdb
 from .models import UserProfile
 
 
 def save_profile(backend, user, response, *args, **kwargs):
-    #pdb.set_trace()
     access_token = response['access_token']
     vk_id = response['user_id']
 
@@ -11,4 +9,8 @@ def save_profile(backend, user, response, *args, **kwargs):
     if not UserProfile.objects.filter(user_id=user.id):
         UserProfile.objects.create(user_id=user.id, token=access_token, vk_id=vk_id)
 
-
+    else:
+        user = UserProfile.objects.get(user_id=user.id)
+        if user.token != access_token:
+            user.token = access_token
+            user.save()
